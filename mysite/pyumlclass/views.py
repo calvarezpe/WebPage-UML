4aa4e7054413c7838aeeaf722f5265dbc750cfa4
Cristian Alvarez <calvarez@MacBook-Pro-de-Cristian.local>
2017-02-28 20:38:18 +0100

    Final version

--- mysite/pyumlclass/views.py
+++ mysite/pyumlclass/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
 from UMLPythonExtractorv2 import proyect
+from django.core.exceptions import ObjectDoesNotExist
+import urllib2
 
 def index(request):
     return render(request, 'pyumlclass/index.html', {})
@@ -14,12 +16,36 @@ def archive(request):
     return render(request, 'pyumlclass/archive.html',context)
 
 def results(request):
-    results_proyect = proyect.Analyze(request.POST['project_url'])
-    print results_proyect
-    response = "You'll see the result of this project: " + request.POST['project_url']
-    response += "\n"
-    return HttpResponse(response)
-    # Falta: Una vez recibida la request, comprobar la base de datos, a ver si se ha analizado ese proyecto
-    # Si se ha analizado se muestra sin llamar al resto del codigo, si no se llama
-    # Hay que comprobar el objeto que se obtiene con la lista
-    # Crear plantilla de resultado para un unico proyecto
+    Page = HttpResponse("Null page")
+    url_project_given = request.POST['project_url']
+    if(len(url_project_given.split("https://github.com")) == 2):
+        project_name_from_url = url_project_given.split("github.com")[-1]
+        # Comprobar si existe el proyecto
+        try:
+            request_api_github = "https://api.github.com/repos" + project_name_from_url
+            f = urllib2.urlopen(request_api_github)
+            try:
+                q = Project.objects.get(name_text=project_name_from_url)
+                # Por aqui sigue si el proyecto se ha analizado
+                context = {'project':q}
+                Page = render(request, 'pyumlclass/one_project.html',context)
+            except ObjectDoesNotExist:
+                # Por aqui sigue si el proyecto no esta en la base de datos
+                results_proyect = proyect.Analyze(url_project_given)
+
+                q = Project(name_text=project_name_from_url,
+                            uml_class=results_proyect[0],
+                            python_class=results_proyect[1],
+                            intersection=results_proyect[2],
+                            without_corrections=results_proyect[3],
+                            hamming_corrections=results_proyect[4],
+                            capital_letters_corrections=results_proyect[5])
+                q.save()
+                context = {'project':q}
+                Page = render(request, 'pyumlclass/one_project.html',context)
+        except urllib2.URLError, e:
+            Page = HttpResponse("The project named " + project_name_from_url + " does not exist.")
+    else:
+        Page = HttpResponse("Link error. Link must be https://github.com/user/project_name")
+
+    return Page
