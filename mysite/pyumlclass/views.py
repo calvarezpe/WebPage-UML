7d42290c58c776faabc791c045f1fface2632359
Cristian Alvarez <calvarez@MacBook-Pro-de-Cristian.local>
2017-03-20 22:34:14 +0100

    new modi

--- mysite/pyumlclass/views.py
+++ mysite/pyumlclass/views.py
@@ -12,6 +12,7 @@ def index(request):
 
 def archive(request):
     projects = Project.objects.all()
+    print("Consulta a la base de datos de todos los proyectos")
     context = {'projects': projects}
     return render(request, 'pyumlclass/archive.html',context)
 
@@ -22,15 +23,19 @@ def results(request):
         project_name_from_url = url_project_given.split("github.com")[-1]
         # Comprobar si existe el proyecto
         try:
+            print("API de GitHub para comprobar existencia proyecto")
             request_api_github = "https://api.github.com/repos" + project_name_from_url
             f = urllib2.urlopen(request_api_github)
             try:
+                print("Consulta a la BBDD sobre el proyecto")
                 q = Project.objects.get(name_text=project_name_from_url)
                 # Por aqui sigue si el proyecto se ha analizado
+                print("Proyecto existente. Se muestran los resultados.")
                 context = {'project':q}
                 Page = render(request, 'pyumlclass/one_project.html',context)
             except ObjectDoesNotExist:
                 # Por aqui sigue si el proyecto no esta en la base de datos
+                print("Proyecto no existente en BBDD. Se analiza el proyecto.")
                 results_proyect = proyect.Analyze(url_project_given)
 
                 q = Project(name_text=project_name_from_url,
@@ -41,6 +46,7 @@ def results(request):
                             hamming_corrections=results_proyect[4],
                             capital_letters_corrections=results_proyect[5])
                 q.save()
+                print("Se incluye a la base de datos y se muestran los resultados")
                 context = {'project':q}
                 Page = render(request, 'pyumlclass/one_project.html',context)
         except urllib2.URLError, e:
